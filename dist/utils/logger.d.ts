b3f6c34ab316a76fa12401a6fa4566ded1c08049
Harshal Raj <hraj1137@gmail.com>
2025-10-25 16:13:53 -0700

    feat: Add logger utility and types

---
+++ dist/utils/logger.d.ts
@@ -0,0 +1,5 @@
+import winston from 'winston';
+export declare const logger: winston.Logger;
+export declare const httpLogger: winston.Logger;
+export declare const errorLogger: winston.Logger;
+//# sourceMappingURL=logger.d.ts.map
---
+++ dist/utils/logger.js
@@ -0,0 +1,68 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errorLogger = exports.httpLogger = exports.logger = void 0;
+const winston_1 = __importDefault(require("winston"));
+const environment_1 = require("../config/environment");
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    debug: 4,
+};
+const colors = {
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+    http: 'magenta',
+    debug: 'white',
+};
+winston_1.default.addColors(colors);
+const level = () => {
+    const env = (0, environment_1.getEnv)('NODE_ENV') || 'development';
+    const logLevel = (0, environment_1.getEnv)('LOG_LEVEL') || 'info';
+    return env === 'development' ? 'debug' : logLevel;
+};
+const format = winston_1.default.format.combine(winston_1.default.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }), winston_1.default.format.colorize({ all: true }), winston_1.default.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`));
+const transports = [
+    new winston_1.default.transports.Console({
+        format: format,
+    }),
+    new winston_1.default.transports.File({
+        filename: 'logs/error.log',
+        level: 'error',
+        format: winston_1.default.format.combine(winston_1.default.format.timestamp(), winston_1.default.format.json()),
+    }),
+    new winston_1.default.transports.File({
+        filename: 'logs/combined.log',
+        format: winston_1.default.format.combine(winston_1.default.format.timestamp(), winston_1.default.format.json()),
+    }),
+];
+exports.logger = winston_1.default.createLogger({
+    level: level(),
+    levels,
+    format,
+    transports,
+});
+exports.httpLogger = winston_1.default.createLogger({
+    level: 'http',
+    format: winston_1.default.format.combine(winston_1.default.format.timestamp(), winston_1.default.format.json()),
+    transports: [
+        new winston_1.default.transports.File({
+            filename: 'logs/http.log',
+        }),
+    ],
+});
+exports.errorLogger = winston_1.default.createLogger({
+    level: 'error',
+    format: winston_1.default.format.combine(winston_1.default.format.timestamp(), winston_1.default.format.json()),
+    transports: [
+        new winston_1.default.transports.File({
+            filename: 'logs/errors.log',
+        }),
+    ],
+});
+//# sourceMappingURL=logger.js.map
